14adefe6b2ea601518f68e04fc317e017eb8f9d2
MomandDadMac2 <MomandDadMac2@FrancesneysiMac.home>
2018-07-14 09:21:34 -0400

    completed API calls

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ var state = {
 };
 
 const googleBooksURL = 'https://www.googleapis.com/books/v1/volumes';
+const newsURL = 'https://newsapi.org/v2/everything';
+const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
 
+//Calls to the APIs
 
-
-// Query Google Books API
 function getGoogleBooksAPIData(userSelectedSearchItem) {
   const params = {
     q: `"${userSelectedSearchItem}"`,
@@ -24,8 +25,6 @@ function getGoogleBooksAPIData(userSelectedSearchItem) {
    renderGoogleBooksAPIData(data.items);
  });
 }
-//GET https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey
-
 
 function getSelectedGoogleBookAPIData(book) {
   var singleBookURL = googleBooksURL + `/` + book + `?key=AIzaSyBpAvj7qUWfzUvniX__WEqh8iN5AUphs6s`;
@@ -34,6 +33,94 @@ function getSelectedGoogleBookAPIData(book) {
    //nextPageToken = data.nextPageToken;
  });
 }
+
+function getYouTubeAPIData(userSelectedSearchItem) {
+  const params = {
+    q: `${userSelectedSearchItem} in:name`,
+    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
+    part: 'snippet',
+    maxResults: 8,
+    videoID:'id'
+  }
+ $.getJSON(youtubeURL, params, function(data){
+   renderYouTubeAPIData(data.items);
+   nextPageToken = data.nextPageToken;
+ });
+}
+
+function getNewsAPIData(userSelectedSearchItem) {
+  const params = {
+    q: `${userSelectedSearchItem}`,
+    apiKey: `430d190071894a52b7716e87bf74ced3`,
+    //language: `en`,
+    //pagesize: `5`
+    }
+  
+    $.getJSON(newsURL, params, function(data){
+      renderNewsAPIData(data.items);
+    });
+}
+
+function renderYouTubeAPIData(data) {
+  console.log(`the following are youtube videos`)
+  console.log(data);
+  $('.dataItem').remove();
+  var indexNum = 0;
+  $.each(data, function(index, item) {
+    indexNum +=1;
+    $(`.js-youtube`).append(`
+      <div class='videoItem'>
+        <h3><div class='js-result-name'>
+           <a href='${item.snippet.title}'>${item.snippet.title}</a>
+        </div></h3>
+        <div class='js-image'>
+         <img data-videoid='${item.id.videoId}' src='${item.snippet.thumbnails.medium.url}' >
+        </div>
+        <div class='js-desc'>
+         <p>${item.snippet.description}</p>
+        </div>
+        <div class='myVideo' id='${indexNum}'>
+            <iframe data-videoIndex = ${index} src='https://www.youtube.com/embed/${item.id.videoId}?controls=1'></iframe>
+        </div>
+      </div>
+      `
+    );
+  });
+}
+
+
+
+
+function renderNewsAPIData(articles) {
+  console.log(`the following are articles on the topic videos`)
+  console.log(articles);
+  $('.newsItem').remove();
+  var indexNum = 0;
+  //var dataId="";
+  $.each(articles, function(index, article) {
+    indexNum +=1;
+    //dataId=`${article.id}`;
+    console.log(article.author);
+
+    $(`.js-news`).append(`
+      <div class='newsItem'>
+           <H3><div class="js-title">${article.articles.title} by ${article.author}</div></h3>
+           <div class="js-subtitle">${article.description}</div>
+           <div class="js-subtitle">${article.publishedAt} from source:  ${article.id}</div>
+           <div class="js-authors"><a href="${article.url}">${article.url}</a></div>
+      </div>
+      `
+    ); 
+  });
+}
+
+
+
+
+// Query Google Books API
+
+
+
 
 // Render the API data to the DOM
 function renderGoogleBooksAPIData(data) {
@@ -58,6 +145,7 @@ function renderGoogleBooksAPIData(data) {
 }
 
 function renderSelectedBookAPIData(book) {
+  console.log(`the following are details on the book selected`)
   console.log(book);
   $('.dataItem').remove();
     $(`.js-Overview`).append(`
@@ -76,10 +164,10 @@ function renderSelectedBookAPIData(book) {
     $('.purchaseDataItem').remove();
     var forSale=`${book.saleInfo.saleability}`;
     var isElectronic = `${book.saleInfo.isEbook}`;
-    console.log('This book is for sale ' + forSale + ' also, this book is electronic ' + isElectronic );
+    //console.log('This book is for sale ' + forSale + ' also, this book is electronic ' + isElectronic );
 
     if (isElectronic === `true`) {
-      console.log('did book make it to here');
+      //console.log('did book make it to here');
         if (forSale == 'FREE') {
           $(`.js-purchase`).append(`
                 <div class='purchaseDataItem'>
@@ -130,6 +218,7 @@ function watchSubmit() {
     const queryTarget = $(e.currentTarget).find('.js-queryByTitle');
     userSelectedSearchTerm  = queryTarget.val();
     getGoogleBooksAPIData(userSelectedSearchTerm);
+    
   });
 }
 
@@ -138,6 +227,8 @@ function listenForBookSelection() {
     e.preventDefault();
     const selectedBookId = $(e.currentTarget).find('.js-itemId').attr('data-bookId');
     getSelectedGoogleBookAPIData(selectedBookId);
+    getNewsAPIData(userSelectedSearchTerm);
+    getYouTubeAPIData(userSelectedSearchTerm)
   }));
 }
 
